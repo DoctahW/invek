4ce2d0796d94c377782d9fcb065f4c5e644b6e34
euclides <joaomartires2@gmail.com>
2026-06-02 16:45:49 -0300

    fix: tubopack bug
    Signed-off-by: euclides <joaomartires2@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: [
+    "better-auth",
+    "@better-auth/kysely-adapter",
+    "kysely",
+  ],
 };
 
 export default nextConfig;
